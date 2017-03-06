02b8a585a0f7cb111f172894f534b121fc2b3255
Christopher Andrews <candrews@middlebury.edu>
2017-03-06 14:46:07 -0500

    Created a simple example demonstrating making the keyboard disappear.
    The keyboard will collapse when the user taps the Done button or when the user taps the view above the keyboard.

--- KeyboardTest/ViewController.swift
+++ KeyboardTest/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+  // note that we made our view controller a UITextFieldDelegate, which basically means it implements a collection of functions our text field might call
+  
+  
+  @IBOutlet weak var textOutput: UILabel!
+  @IBOutlet weak var textInput: UITextField!
+  
+  
+  // I added a Tap Gesture Recognizer to the view
+  // this action is fired by tapping on the view 
+  // the effect is that when the keyboard is showing, tapping outside of the keyboard makes it go away
+  @IBAction func tapToDismissKeyboard(_ sender: UITapGestureRecognizer) {
+    textInput.resignFirstResponder() //hide the keyboard
 
+  }
+  
+  @IBAction func textChanged(_ sender: UITextField) {
+    textOutput.text = textInput.text
+  }
+  
+  // This is a delegate method
+  // The text field will call this when the done/return/etc button is typed
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder() //hide the keyboard
+    return true
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    // make sure that this view controller is a delegate for the text field
+    textInput.delegate = self
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
+ 
 
 }
 
